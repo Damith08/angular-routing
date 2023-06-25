ba43e9db79686128babafc859f640fd819cba1fa
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-06-25 16:06:03 +0530

    feat: location strategies
    Section 11 - Understanding Location Strategies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -10,14 +11,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRoutingModel } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
-import { ServerResolver } from './servers/server-resolver.service';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 @NgModule({
   declarations: [
@@ -29,16 +29,14 @@ import { ServerResolver } from './servers/server-resolver.service';
     EditServerComponent,
     ServerComponent,
     PageNotFoundComponent,
-    ErrorPageComponent,
+    ErrorPageComponent
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModel],
-  providers: [
-    ServersService,
-    AuthService,
-    AuthGuard,
-    CanDeactivateGuard,
-    ServerResolver,
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  bootstrap: [AppComponent],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard, ServerResolver],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
